927da2f59423bfb388aa5646f3b3cb9741ba2acc
Steven Bapaga <sbapaga@gmail.com>
2017-04-03 16:29:35 +0100

    CSRA-260: Decouple server binding configuration from app
    - Add test watch command
    - Fix bug in which test would not run on a file which wasn't in a folder

--- server/app.js
+++ server/app.js
@@ -1,21 +1,11 @@
-const http = require('http');
-const path = require('path');
+import path from 'path';
+import express from 'express';
 
-const express = require('express');
-
-// config parsing
-const env = process.env;
-const port = env.PORT || '5000';
 
 // Express app
 const app = express();
 
-
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
-// actually listening
-const server = http.createServer(app);
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`listening on http://localhost:${port}`);
-});
+
+export default app;
