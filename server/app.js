8a87b495fa5597803b89d6bffaf1dbecf038732d
Steven Bapaga <sbapaga@gmail.com>
2017-04-03 16:31:02 +0100

    CSRA-256: Add /health endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import path from 'path';
 import express from 'express';
 
+import healthRoute from './routes/health';
+
+import { HEALTH_ENDPOINT } from './constants/routes';
 
 // Express app
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
-
+app.use(HEALTH_ENDPOINT, healthRoute);
 
 export default app;
---
+++ test/server/routes/health.spec.js
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import express from 'express';
+import healthEndpoint from '../../../server/routes/health';
+
+describe('GET /health', () => {
+  let app;
+  before(() => {
+    app = express();
+    app.use('/health', healthEndpoint);
+  });
+
+  it('respond with { status: "OK" }', (done) => {
+    request(app)
+      .get('/health')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '15')
+      .expect(200, {
+        status: 'OK',
+      }, done);
+  });
+});
