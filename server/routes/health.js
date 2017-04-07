39aed4ed225eb7501ad6e036686169f7b4a884c1
Glen Mailer <glenjamin@gmail.com>
2017-04-07 11:03:39 +0100

    Add build info as extra commit when deploying

--- server/routes/health.js
+++ server/routes/health.js
@@ -2,9 +2,20 @@ import express from 'express';
 
 const router = express.Router();
 
-/* GET home page. */
+function getBuildInfo() {
+  try {
+    // eslint-disable-next-line global-require,import/no-unresolved
+    return require('../../build-info.json');
+  } catch (ex) {
+    return {};
+  }
+}
+
 router.get('/', (req, res) => {
-  res.status(200).json({ status: 'OK' });
+  res.status(200).json({
+    status: 'OK',
+    ...getBuildInfo(),
+  });
 });
 
 export default router;
