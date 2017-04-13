83d36f3f6b66bbd51dee105f42104cfcd7048395
Steven Bapaga <sbapaga@gmail.com>
2017-04-13 13:03:59 +0100

    CSRA-347: Add test coverage on health endpoint for build-info

--- test/unit/server/routes/health.spec.js
+++ test/unit/server/routes/health.spec.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import fs from 'fs';
+import rimraf from 'rimraf';
 import request from 'supertest';
 import express from 'express';
 import healthEndpoint from '../../../../server/routes/health';
@@ -13,9 +16,35 @@ describe('GET /health', () => {
     request(app)
       .get('/health')
       .expect('Content-Type', /json/)
-      .expect('Content-Length', '15')
       .expect(200, {
         status: 'OK',
       }, done);
+  });
+
+  context('when the build-info.json file is present', () => {
+    const filePath = path.resolve(__dirname, '../../../../build-info.json');
+
+    before(() => {
+      const buildInfo = { buildNumber: 'foo', gitRef: 'bar' };
+      fs.writeFileSync(filePath, JSON.stringify(buildInfo), { encoding: 'utf-8' });
+    });
+
+    after(() => {
+      rimraf.sync(filePath);
+    });
+
+    it('responds with the build information inside the status ended', (done) => {
+      const server = express();
+      server.use('/health', healthEndpoint);
+
+      request(app)
+      .get('/health')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        status: 'OK',
+        buildNumber: 'foo',
+        gitRef: 'bar',
+      }, done);
+    });
   });
 });
