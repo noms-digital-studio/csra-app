41b4fad54737f9414cf92ebf5fb180c99fcbf09b
Steven Bapaga <sbapaga@gmail.com>
2017-05-15 11:12:32 +0100

    CSRA-000: Fix broken pipeline

--- test/end-to-end/browser/homepage.spec.js
+++ test/end-to-end/browser/homepage.spec.js
@@ -7,7 +7,7 @@ describe('/ (homepage)', () => {
     expect(browser.getTitle()).equal('Assess if a prisoner can share a cell safely');
   });
 
-  it('displays "Enter your full name" on the page', () => {
-    expect(browser.getText('main')).to.contain('Enter your full name');
+  it('displays "Your full name" on the page', () => {
+    expect(browser.getText('main')).to.contain('Your full name');
   });
 });
