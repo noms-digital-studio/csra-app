5317f58b77308b55a511033de9789d26bad1dc77
Steven Bapaga <sbapaga@gmail.com>
2017-05-15 11:56:51 +0100

    CSRA-000: Remove browser pause

--- test/end-to-end/rest/health.spec.js
+++ test/end-to-end/rest/health.spec.js
@@ -2,8 +2,7 @@ import request from 'supertest';
 import { expect } from 'chai';
 
 describe('/health', () => {
-  it('displays the health status of the app', function test(done) {
-    this.timeout(5000);
+  it('displays the health status of the app', (done) => {
     const baseUrl = process.env.APP_BASE_URL;
     request(baseUrl).get('/health')
       .expect(200)
