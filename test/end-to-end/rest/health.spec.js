2f48c9a16d7db874fc218de69de31c1a16c82f90
Steven Bapaga <sbapaga@gmail.com>
2017-05-15 12:00:02 +0100

    CSRA-000: Undo Previous commit

--- test/end-to-end/rest/health.spec.js
+++ test/end-to-end/rest/health.spec.js
@@ -2,7 +2,8 @@ import request from 'supertest';
 import { expect } from 'chai';
 
 describe('/health', () => {
-  it('displays the health status of the app', (done) => {
+  it('displays the health status of the app', function test(done) {
+    this.timeout(5000);
     const baseUrl = process.env.APP_BASE_URL;
     request(baseUrl).get('/health')
       .expect(200)
