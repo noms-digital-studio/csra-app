6813bc10fbe0a4d6ac191e42a24c038a75bff43f
Steven Bapaga <sbapaga@gmail.com>
2017-05-23 12:16:52 +0100

    CSRA-000: Update deployment scripts

--- kudu-deploy.sh
+++ kudu-deploy.sh
@@ -131,8 +131,7 @@ selectNodeVersion
 installYarn
 
 echo "Installing npm packages via yarn"
-# unset NODE_ENV so we get dev dependencies included
-NODE_ENV= yarn.cmd install --frozen-lockfile
+yarn.cmd install --frozen-lockfile
 exitWithMessageOnError "yarn install failed"
 
 cd - > /dev/null
