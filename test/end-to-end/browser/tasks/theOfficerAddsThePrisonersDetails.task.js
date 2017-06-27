1739b22f42f5f46d62e88af7d2c05149c7e00f08
Nick Redshaw <nickredshaw@gmail.com>
2017-06-27 12:42:10 +0100

    CSRA-493: Add prisoner via Add Prisoner Page and prime viper score in database

--- test/end-to-end/browser/tasks/theOfficerAddsThePrisonersDetails.task.js
+++ test/end-to-end/browser/tasks/theOfficerAddsThePrisonersDetails.task.js
@@ -7,13 +7,14 @@ function whenTheOfficerAddsThePrisonersDetails(nomisId) {
   expect(AddPrisonerPage.mainHeading).to.equal('Add Prisoner');
   AddPrisonerPage.enterName('John', 'Lowe');
   AddPrisonerPage.enterDoB('01', '10', '1970');
-  AddPrisonerPage.enterNomisId(nomisId || 'A12345');
+  AddPrisonerPage.enterNomisId(nomisId || 'J1234LO');
   AddPrisonerPage.clickAddPrisonerButton();
   expect(PrisonerAddedPage.mainHeading).to.equal('Prisoner Added');
   expect(PrisonerAddedPage.name).to.equal('John Lowe');
   expect(PrisonerAddedPage.dob).to.equal('01-10-1970');
-  expect(PrisonerAddedPage.nomisId).to.equal(nomisId || 'A12345');
+  expect(PrisonerAddedPage.nomisId).to.equal(nomisId || 'J1234LO');
   PrisonerAddedPage.clickConfirm();
+  DashboardPage.waitForMainHeadingWithDataId('dashboard');
 }
 
 export default whenTheOfficerAddsThePrisonersDetails;
