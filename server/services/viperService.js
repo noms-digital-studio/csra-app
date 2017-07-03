25fbbe79d7dc5351fc3eea17e4b7ea72e7eabbf3
Nick Redshaw <nickredshaw@gmail.com>
2017-07-03 13:32:36 +0100

    CSRA-505: Viper API integration

--- server/services/viperService.js
+++ server/services/viperService.js
@@ -1,4 +1,8 @@
-function ratingFor(db, nomisId) {
+/* eslint-disable no-console */
+import superagent from 'superagent';
+
+function viperRatingFromDatabase(db, nomisId) {
+  console.log('Getting viper rating from the database for nomisID: ', nomisId);
   return db
     .select()
     .table('viper')
@@ -8,8 +12,34 @@ function ratingFor(db, nomisId) {
         return result[0].rating;
       }
 
+      console.log('viper rating not found for nomisID: ', nomisId);
       return null;
     });
+}
+
+function viperRatingFromApi(nomisId) {
+  console.log('Getting viper rating from the REST API for nomisID: ', nomisId);
+  superagent
+    .get(`http://localhost:9090/offender/${nomisId}/viper`)
+    .set('Ocp-Apim-Subscription-Key', 'valid-subscription-key')
+    .end((error, res) => {
+      if (error) {
+        console.log('viper rating not found for nomisID: ', nomisId);
+        console.log('error: ', error);
+        return null;
+      }
+
+      console.log('viper rating found for nomisId: ', nomisId);
+      return res.body.viperRating;
+    });
+}
+
+function ratingFor(db, nomisId) {
+  if (process.env.USE_VIPER_REST_API) {
+    return viperRatingFromApi(nomisId);
+  }
+
+  return viperRatingFromDatabase(db, nomisId);
 }
 
 export default function createViperService(db) {
--- test/end-to-end/rest/viper.spec.js
+++ test/end-to-end/rest/viper.spec.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 import request from 'supertest';
 import uuid from 'uuid/v4';
 import db from '../../util/db';
+import superagent from 'superagent';
 
 const baseUrl = process.env.APP_BASE_URL;
 
@@ -14,14 +16,44 @@ describe('/api/viper/:nomisId', () => {
     })
       .into('viper')
       .then(
+        superagent
+          .post('http://localhost:9090/__admin/mappings')
+          .send({
+            request: {
+              method: 'GET',
+              urlPattern: `/offender/${nomisId}/viper`,
+              headers: {
+                'Ocp-Apim-Subscription-Key': {
+                  equalTo: 'valid-subscription-key',
+                },
+              },
+            },
+            response: {
+              status: 200,
+              body: `{"nomsId": "${nomisId}", "viperRating": 0.42}`,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .end((error, res) => {
+            if (error) {
+              console.log('error: ', error);
+            }
+
+            console.log('Wiremock primed with nomisId: ', nomisId);
+          }),
+
+
         request(baseUrl).get(`/api/viper/${nomisId}`)
           .expect(200)
           .end((err, res) => {
             if (err) done(err);
-            expect(res.body).to.eql({ nomisId, viperRating: 0.42 });
+            expect(res.body).to.eql({nomisId, viperRating: 0.42});
             done();
           }),
-      );
+      )
+    ;
   });
 
   it('returns a 404 (not found) for an unknown nomis id', function test(done) {
@@ -31,7 +63,7 @@ describe('/api/viper/:nomisId', () => {
       .expect(404)
       .end((err, res) => {
         if (err) done(err);
-        expect(res.body).to.eql({ messasge: `Error retrieving viper rating for nomisId: ${nomisId}. The cause was: Not found` });
+        expect(res.body).to.eql({messasge: `Error retrieving viper rating for nomisId: ${nomisId}. The cause was: Not found`});
         done();
       });
   });
