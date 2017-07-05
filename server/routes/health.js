6b4877a7bda4727c5e695b100cab0933311d40f8
Nick Redshaw <nickredshaw@gmail.com>
2017-07-05 17:08:04 +0100

    CSRA-505: Refactor health router before adding the viper service check

--- server/routes/health.js
+++ server/routes/health.js
@@ -4,33 +4,30 @@ export default function createRouter(db, appInfo) {
   const router = express.Router();
 
   router.get('/', (req, res) => {
-    const result = {
-      status: 'OK',
-      ...appInfo.getBuildInfo(),
-      checks: {},
-    };
+    function dbCheck() {
+      return db.select(db.raw('1'))
+        .then(() => ({ name: 'db', status: 'OK', message: 'OK' }))
+        .catch(err => ({ name: 'db', status: 'ERROR', message: err.message }));
+    }
 
-    const checks = {
-      db: db.select(db.raw('1')),
-    };
+    const checks = [dbCheck];
 
-    function handleCheck(checkName) {
-      return checks[checkName]
-        .then(
-          () => {
-            result.checks[checkName] = 'OK';
-          },
-          (err) => {
-            result.status = 'ERROR';
-            result.checks[checkName] = err.message;
-          },
-        );
+    function gatherCheckInfo(total, currentValue) {
+      return { [currentValue.name]: currentValue.message };
     }
 
     Promise
-      .all(Object.keys(checks).map(handleCheck))
-      .then(() => {
-        res.status(result.status === 'OK' ? 200 : 500);
+      .all(checks.map(fn => fn()))
+      .then((checkResults) => {
+        const allOk = checkResults.every(item => item.status === 'OK');
+        const checkData = checkResults.reduce(gatherCheckInfo, {});
+
+        const result = {
+          status: allOk ? 'OK' : 'ERROR',
+          ...appInfo.getBuildInfo(),
+          checks: checkData,
+        };
+        res.status(allOk ? 200 : 500);
         res.json(result);
       });
   });
