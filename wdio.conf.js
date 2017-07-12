5d5f2cddbef513d2d57b4301c2bc795c1c1b2a5b
Steven Bapaga <sbapaga@gmail.com>
2017-07-12 16:37:51 +0100

    CSRA-000: Increase noise in circle ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -60,7 +60,7 @@ exports.config = {
   sync: true,
   //
   // Level of logging verbosity: silent | verbose | command | data | result | error
-  logLevel: 'error',
+  logLevel: 'command',
   //
   // Enables colors for log output.
   coloredLogs: true,
