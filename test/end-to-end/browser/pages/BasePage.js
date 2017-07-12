06e19fabe77e7e91e9a439ccfe3fd524e31ab7e8
Steven Bapaga <sbapaga@gmail.com>
2017-07-12 17:26:42 +0100

    CSRA-000: Reset timers back to default

--- test/end-to-end/browser/pages/BasePage.js
+++ test/end-to-end/browser/pages/BasePage.js
@@ -3,17 +3,14 @@ class BasePage {
 
   get mainHeading() { return browser.getText('h1'); }
   waitForMainHeadingWithDataId(id) {
-    browser.waitForVisible(`[data-title="${id}"]`, 3000);
+    browser.waitForVisible(`[data-title="${id}"]`, 5000);
     return browser.getText('h1');
   }
   get form() { return browser.element('.form'); }
   get headerUsername() { return browser.getText('[data-header-username]'); }
 
   submitPage() { this.form.submitForm(); }
-  clickContinue() {
-    browser.pause(3000);
-    browser.click('[data-continue-button]');
-  }
+  clickContinue() { browser.click('[data-continue-button]'); }
 }
 
 export default BasePage;
