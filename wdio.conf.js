dcd7649b125206456ed4764aac8c9e61def3c5b6
Steven Bapaga <sbapaga@gmail.com>
2017-07-13 14:47:36 +0100

    Reduce maxinstances of browser

--- wdio.conf.js
+++ wdio.conf.js
@@ -43,7 +43,7 @@ exports.config = {
       // maxInstances can get overwritten per capability. So if you have an in-house Selenium
       // grid with only 5 firefox instance available you can make sure that not more than
       // 5 instance gets started at a time.
-      maxInstances: 5,
+      maxInstances: 1,
       //
       browserName: process.env.E2E_BROWSER_NAME || 'phantomjs', // options: chrome || firefox || phantomjs
     },
