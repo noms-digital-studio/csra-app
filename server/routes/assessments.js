155f159530c8fab192fe49e644ded54289b92e0c
Nick Redshaw <nickredshaw@gmail.com>
2017-07-24 16:59:35 +0100

    CSRA-547: Add routers for updating and retrieving the risk assessment

--- server/routes/assessments.js
+++ server/routes/assessments.js
@@ -43,5 +43,59 @@ export default function createRouter(prisonerAssessmentsService) {
       });
   });
 
+  router.post('/:id/risk', (req, res) => {
+    const id = req.params.id;
+
+    prisonerAssessmentsService.saveRiskAssessment(id, req.body)
+    .then(() => {
+      res.status(200);
+      res.json();
+    }).catch((err) => {
+      log.error(err);
+      if (err.type === 'validation') {
+        res.status(400);
+        res.json({
+          status: 'VALIDATION ERROR',
+          message: err.message,
+        });
+      } else if (err.type === 'not-found') {
+        res.status(404);
+        res.json({
+          status: 'NOT FOUND',
+          message: err.message,
+        });
+      }
+      res.status(500);
+      res.json({
+        status: 'ERROR',
+        message: err.message,
+      });
+    });
+  });
+
+  router.get('/:id/risk', (req, res) => {
+    const id = req.params.id;
+
+    prisonerAssessmentsService.getRiskAssessmentForId(id)
+    .then((result) => {
+      res.status(200);
+      res.json(result);
+    }).catch((err) => {
+      log.error(err);
+      if (err.type === 'not-found') {
+        res.status(404);
+        res.json({
+          status: 'NOT FOUND',
+          message: err.message,
+        });
+      }
+      res.status(500);
+      res.json({
+        status: 'ERROR',
+        message: err.message,
+      });
+    });
+  });
+
   return router;
 }
--- test/unit/server/routes/assessments.spec.js
+++ test/unit/server/routes/assessments.spec.js
@@ -3,21 +3,17 @@ import sinon from 'sinon';
 import express from 'express';
 import createPrisonerAssessmentsEndpoint from '../../../../server/routes/assessments';
 
+const app = express();
+
+const fakePrisonerAssessmentsService = sinon.stub();
+app.use(createPrisonerAssessmentsEndpoint(fakePrisonerAssessmentsService));
+
 describe('POST /assessements', () => {
-  let app;
-  let fakePrisonerAssessmentsService;
-
-  beforeEach(() => {
-    app = express();
-    fakePrisonerAssessmentsService = sinon.stub();
-    app.use('/assessments', createPrisonerAssessmentsEndpoint(fakePrisonerAssessmentsService));
-  });
-
   it('responds with status CREATED (201) when the prisoner assessment record was created', () => {
     fakePrisonerAssessmentsService.save = sinon.stub().resolves({ id: 123 });
 
     return request(app)
-      .post('/assessments')
+      .post('/')
       .send({
         nomisId: 'AS223213',
         forename: 'John',
@@ -37,7 +33,7 @@ describe('POST /assessements', () => {
     fakePrisonerAssessmentsService.save = sinon.stub().rejects(err);
 
     return request(app)
-      .post('/assessments')
+      .post('/')
       .send({
         nomisId: 'AS223213',
         forename: 'John',
@@ -55,7 +51,7 @@ describe('POST /assessements', () => {
     fakePrisonerAssessmentsService.save = sinon.stub().rejects(new Error('Terrible database error'));
 
     return request(app)
-      .post('/assessments')
+      .post('/')
       .send({
         nomisId: 'AS223213',
         forename: 'John',
@@ -71,15 +67,6 @@ describe('POST /assessements', () => {
 });
 
 describe('GET /assessements', () => {
-  let app;
-  let fakePrisonerAssessmentsService;
-
-  beforeEach(() => {
-    app = express();
-    fakePrisonerAssessmentsService = sinon.stub();
-    app.use('/assessments', createPrisonerAssessmentsEndpoint(fakePrisonerAssessmentsService));
-  });
-
   it('responds with status OK (200) and the list of prisoner assessment summaries', () => {
     const assessmentSummaryList = [{
       id: 123,
@@ -94,7 +81,7 @@ describe('GET /assessements', () => {
     fakePrisonerAssessmentsService.list = sinon.stub().resolves(assessmentSummaryList);
 
     return request(app)
-      .get('/assessments')
+      .get('/')
       .expect(200)
       .expect('Content-Type', /json/)
       .expect((res) => {
@@ -106,9 +93,116 @@ describe('GET /assessements', () => {
     fakePrisonerAssessmentsService.list = sinon.stub().rejects(new Error('Terrible database error'));
 
     return request(app)
-      .get('/assessments')
+      .get('/')
       .expect(500)
       .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql({ status: 'ERROR', message: 'Terrible database error' });
+    });
+  });
+});
+
+describe('POST /assessements/:id/risk', () => {
+  it('Returns OK (200) when the prisoner assessment is updated with the risk assessment data', () => {
+    fakePrisonerAssessmentsService.saveRiskAssessment = sinon.stub().resolves();
+
+    return request(app)
+    .post('/123/risk')
+    .send({
+      riskAssessment: { someKey: 'some valid data' },
+    })
+    .expect(200)
+    .expect('Content-Type', /json/);
+  });
+
+  it('responds with status BAD REQUEST (400) and an error message when the service indicates an validation error', () => {
+    const err = new Error('Foo was not valid');
+    err.type = 'validation';
+    fakePrisonerAssessmentsService.saveRiskAssessment = sinon.stub().rejects(err);
+
+    return request(app)
+    .post('/123/risk')
+    .send({
+      riskAssessment: { someKey: 'some bad data' },
+    })
+    .expect(400)
+    .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql({ status: 'VALIDATION ERROR', message: 'Foo was not valid' });
+    });
+  });
+
+  it('responds with status NOT FOUND (404) and an error message when the assessment id cannot be found', () => {
+    const err = new Error('Assessment not found');
+    err.type = 'not-found';
+    fakePrisonerAssessmentsService.saveRiskAssessment = sinon.stub().rejects(err);
+
+    return request(app)
+    .post('/123/risk')
+    .send({
+      riskAssessment: { someKey: 'some valid data' },
+    })
+    .expect(404)
+    .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql({ status: 'NOT FOUND', message: 'Assessment not found' });
+    });
+  });
+
+  it('responds with status SERVER ERROR (500) and an error message when the service is unable to save the data', () => {
+    fakePrisonerAssessmentsService.saveRiskAssessment = sinon.stub().rejects(new Error('Terrible database error'));
+
+    return request(app)
+    .post('/123/risk')
+    .send({
+      riskAssessment: { someKey: 'some valid data' },
+    })
+    .expect(500)
+    .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql({ status: 'ERROR', message: 'Terrible database error' });
+    });
+  });
+});
+
+describe('GET /assessements/:id/risk', () => {
+  it('responds with status OK (200) and the risk assessment', () => {
+    const riskAssessment = {
+      riskAssessment: { someKey: 'some valid data' },
+    };
+
+    fakePrisonerAssessmentsService.getRiskAssessmentForId = sinon.stub().resolves(riskAssessment);
+
+    return request(app)
+    .get('/:id/risk')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql(riskAssessment);
+    });
+  });
+
+  it('responds with status NOT FOUND (404) and an error message when the assessment id cannot be found', () => {
+    const err = new Error('Assessment not found');
+    err.type = 'not-found';
+    fakePrisonerAssessmentsService.getRiskAssessmentForId = sinon.stub().rejects(err);
+
+    return request(app)
+    .get('/123/risk')
+    .expect(404)
+    .expect('Content-Type', /json/)
+    .expect((res) => {
+      expect(res.body).to.eql({ status: 'NOT FOUND', message: 'Assessment not found' });
+    });
+  });
+
+  it('responds with status OK (500) and an error message when the service is unable to fetch the data ', () => {
+    fakePrisonerAssessmentsService.getRiskAssessmentForId = sinon.stub().rejects(new Error('Terrible database error'));
+
+    return request(app)
+    .get('/123/risk')
+    .expect(500)
+    .expect('Content-Type', /json/)
     .expect((res) => {
       expect(res.body).to.eql({ status: 'ERROR', message: 'Terrible database error' });
     });
