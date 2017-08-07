ec07738f933485f72dd771dbe08e39c1e92768c5
Steven Bapaga <sbapaga@gmail.com>
2017-08-07 15:05:09 +0100

    CSRA-000: Fix outcome page checking data before it's available

--- client/javascript/pages/FullAssessmentOutcome.jsx
+++ client/javascript/pages/FullAssessmentOutcome.jsx
@@ -2,9 +2,9 @@ import React, { PropTypes, Component } from 'react';
 import DocumentTitle from 'react-document-title';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
-import { browserHistory } from 'react-router';
 import not from 'ramda/src/not';
 import path from 'ramda/src/path';
+import isEmpty from 'ramda/src/isEmpty';
 
 import { cellAssignment } from '../services';
 import getAssessmentsById from '../services/getFullOutcome';
@@ -38,9 +38,8 @@ class FullAssessmentOutcome extends Component {
     });
   }
 
-  render() {
+  renderFullOutcome() {
     const {
-      title,
       prisoner,
       riskAssessment,
       healthAssessment,
@@ -60,81 +59,91 @@ class FullAssessmentOutcome extends Component {
     });
 
     return (
+      <div>
+        <div className="grid-row">
+          <div className="column-two-thirds">
+            <h1 data-title="full-outcome" className="heading-xlarge">
+              Risk and healthcare assessment outcome
+            </h1>
+          </div>
+          <div className="column-one-third">
+            <div className="c-print-link u-text-align-right">
+              <button type="button" className="c-icon-button link" onClick={() => window.print()}>
+                Print Page
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="u-margin-bottom-bravo">
+          <PrisonerProfile {...prisoner} />
+        </div>
+
+        <div className="panel panel-border-wide">
+          <h3 className="heading-large" data-element-id="recommended-outcome">
+            Recommended outcome: {capitalize(finalOutcome)}
+          </h3>
+          {riskAssessment.reasons &&
+            <ul data-element-id="reasons" className="list list-bullet">
+              {riskAssessment.reasons.map((item, index) =>
+                <li key={index}>
+                  {item.reason}
+                </li>,
+              )}
+            </ul>}
+        </div>
+
+        <div data-element-id="risk-assessment-summary">
+          <RiskAssessmentSummaryTable title="Risk assessment summary" />
+        </div>
+
+        <div data-element-id="health-summary" className="u-margin-bottom-bravo">
+          <HealthcareSummaryTable title="Healthcare assessment summary" />
+        </div>
+
+        {alreadyCompleted
+          ? <button
+            className="button"
+            type="button"
+            data-element-id="continue-button"
+            to={routes.DASHBOARD}
+            onClick={onReturnHome}
+          >
+              Return to Dashboard
+            </button>
+          : <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSubmit({ assessmentId: prisoner.id, outcome: finalOutcome });
+            }}
+          >
+            <div className="u-clear-fix u-margin-bottom-charlie">
+              <SelectableInput
+                required
+                type="checkbox"
+                id="confirmation"
+                value="accepted"
+                text="The outcome has been explained and the prisoner understands."
+                name="confirmation"
+              />
+            </div>
+
+            <button className="button" type="submit" data-element-id="continue-button">
+                Complete Assessment
+              </button>
+          </form>}
+      </div>
+    );
+  }
+
+  render() {
+    const { title, riskAssessment, healthAssessment } = this.props;
+    const assessmentIsAvailable = not(isEmpty(riskAssessment)) && not(isEmpty(healthAssessment));
+
+    return (
       <DocumentTitle title={title}>
         <div>
-          <div className="grid-row">
-            <div className="column-two-thirds">
-              <h1 data-title="full-outcome" className="heading-xlarge">
-                Risk and healthcare assessment outcome
-              </h1>
-            </div>
-            <div className="column-one-third">
-              <div className="c-print-link u-text-align-right">
-                <button type="button" className="c-icon-button link" onClick={() => window.print()}>
-                  Print Page
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="u-margin-bottom-bravo">
-            <PrisonerProfile {...prisoner} />
-          </div>
-
-          <div className="panel panel-border-wide">
-            <h3 className="heading-large" data-element-id="recommended-outcome">
-              Recommended outcome: {capitalize(finalOutcome)}
-            </h3>
-            {riskAssessment.reasons &&
-              <ul data-element-id="reasons" className="list list-bullet">
-                {riskAssessment.reasons.map((item, index) =>
-                  <li key={index}>
-                    {item.reason}
-                  </li>,
-                )}
-              </ul>}
-          </div>
-
-          <div data-element-id="risk-assessment-summary">
-            <RiskAssessmentSummaryTable title="Risk assessment summary" />
-          </div>
-
-          <div data-element-id="health-summary" className="u-margin-bottom-bravo">
-            <HealthcareSummaryTable title="Healthcare assessment summary" />
-          </div>
-
-          {alreadyCompleted
-            ? <button
-              className="button"
-              type="button"
-              data-element-id="continue-button"
-              to={routes.DASHBOARD}
-              onClick={onReturnHome}
-            >
-                Return to Dashboard
-              </button>
-            : <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                onSubmit({ assessmentId: prisoner.id, outcome: finalOutcome });
-              }}
-            >
-              <div className="u-clear-fix u-margin-bottom-charlie">
-                <SelectableInput
-                  required
-                  type="checkbox"
-                  id="confirmation"
-                  value="accepted"
-                  text="The outcome has been explained and the prisoner understands."
-                  name="confirmation"
-                />
-              </div>
-
-              <button className="button" type="submit" data-element-id="continue-button">
-                  Complete Assessment
-                </button>
-            </form>}
-
+          {assessmentIsAvailable && this.renderFullOutcome()}
         </div>
       </DocumentTitle>
     );
