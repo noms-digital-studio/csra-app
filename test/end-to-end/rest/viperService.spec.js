c2fe171cb3220c373ae874a0c67735fd0e243d40
Steven Bapaga <sbapaga@gmail.com>
2017-08-07 10:52:09 +0100

    CSRA-000: Reformat code and remove fat arrow function in the before

--- test/end-to-end/rest/viperService.spec.js
+++ test/end-to-end/rest/viperService.spec.js
@@ -9,17 +9,16 @@ import config from '../../../server/config';
 const baseUrl = process.env.APP_BASE_URL;
 
 function primeDatabase(nomisId) {
-  return db.insert({
-    nomis_id: nomisId,
-    rating: 0.42,
-  })
+  return db
+    .insert({
+      nomis_id: nomisId,
+      rating: 0.42,
+    })
     .into('viper');
 }
 
 function primeMock(mapping) {
-  return superagent
-    .post(url.resolve(`${config.viper.url}`, '/__admin/mappings'))
-    .send(mapping);
+  return superagent.post(url.resolve(`${config.viper.url}`, '/__admin/mappings')).send(mapping);
 }
 
 function generateNomisId() {
@@ -56,21 +55,21 @@ describe('/api/viper/:nomisId', () => {
     });
   });
 
-  after(() => superagent
-      .post(url.resolve(`${config.viper.url}`, '/__admin/reset'))
-      .send());
+  after(() => {
+    if (!config.viper.enabled) {
+      superagent.post(url.resolve(`${config.viper.url}`, '/__admin/reset')).send();
+    }
+  });
 
   it('returns a viper rating for a known nomis id', function test() {
     this.timeout(5000);
-    return request(baseUrl).get(`/api/viper/${nomisId}`)
-      .expect(200)
-      .then((res) => {
-        expect(res.body).to.eql({ nomisId, viperRating: 0.42 });
-      });
+    return request(baseUrl).get(`/api/viper/${nomisId}`).expect(200).then((res) => {
+      expect(res.body).to.eql({ nomisId, viperRating: 0.42 });
+    });
   });
 
   context('when using actual API', () => {
-    before(() => {
+    before(function test() {
       if (!config.viper.enabled) {
         this.skip();
       }
@@ -96,14 +95,11 @@ describe('/api/viper/:nomisId', () => {
             'Content-Type': 'application/json',
           },
         },
-      })
-        .then(() =>
-          request(baseUrl).get(`/api/viper/${newNomisId}`)
-            .expect(200)
-            .then((res) => {
-              expect(res.body).to.eql({ nomisId: newNomisId, viperRating: 0.42 });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get(`/api/viper/${newNomisId}`).expect(200).then((res) => {
+          expect(res.body).to.eql({ nomisId: newNomisId, viperRating: 0.42 });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when an unauthorised (401) response is received', function test() {
@@ -115,17 +111,19 @@ describe('/api/viper/:nomisId', () => {
         },
         response: {
           status: 401,
-          body: '{"code": "Unauthorised", "message": "Unauthorized; User or application must authenticate"}',
+          body:
+            '{"code": "Unauthorised", "message": "Unauthorized; User or application must authenticate"}',
           headers: {
             'Content-Type': 'application/json',
           },
         },
       })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-401')
-            .expect(404),
-        ).then((res) => {
-          expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-401. The cause was: Not found' });
+        .then(() => request(baseUrl).get('/api/viper/foo-401').expect(404))
+        .then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-401. The cause was: Not found',
+          });
         });
     });
 
@@ -138,19 +136,20 @@ describe('/api/viper/:nomisId', () => {
         },
         response: {
           status: 403,
-          body: '{"code": "Forbidden","message": "Forbidden; User not authorized to take this action"}',
+          body:
+            '{"code": "Forbidden","message": "Forbidden; User not authorized to take this action"}',
           headers: {
             'Content-Type': 'application/json',
           },
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-403')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-403. The cause was: Not found' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-403').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-403. The cause was: Not found',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when a bad request (400) response is received', function test() {
@@ -167,14 +166,14 @@ describe('/api/viper/:nomisId', () => {
             'Content-Type': 'application/json',
           },
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-400')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-400. The cause was: Not found' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-400').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-400. The cause was: Not found',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when an unexpected error (500) response is received', function test() {
@@ -191,14 +190,14 @@ describe('/api/viper/:nomisId', () => {
             'Content-Type': 'application/json',
           },
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-500')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-500. The cause was: Not found' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-500').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-500. The cause was: Not found',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when an empty response is received', function test() {
@@ -211,14 +210,14 @@ describe('/api/viper/:nomisId', () => {
         response: {
           fault: 'EMPTY_RESPONSE',
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-empty')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-empty. The cause was: Not found' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-empty').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-empty. The cause was: Not found',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when connection is closed during response', function test() {
@@ -231,14 +230,14 @@ describe('/api/viper/:nomisId', () => {
         response: {
           fault: 'RANDOM_DATA_THEN_CLOSE',
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-closed')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-closed. The cause was: Not found' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-closed').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-closed. The cause was: Not found',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when response is malformed', function test() {
@@ -251,14 +250,13 @@ describe('/api/viper/:nomisId', () => {
         response: {
           fault: 'MALFORMED_RESPONSE_CHUNK',
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-mal')
-            .expect(404)
-            .then((res) => {
-              expect(res.body.messasge).to.contain('Error retrieving viper rating for nomisId: foo-mal. The cause was: Invalid body:');
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-mal').expect(404).then((res) => {
+          expect(res.body.messasge).to.contain(
+            'Error retrieving viper rating for nomisId: foo-mal. The cause was: Invalid body:',
+          );
+        }),
+      );
     });
 
     it('returns a 404 (not found) when response body is invalid', function test() {
@@ -276,12 +274,12 @@ describe('/api/viper/:nomisId', () => {
           },
         },
       })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-invalid')
-            .expect(404),
-        )
+        .then(() => request(baseUrl).get('/api/viper/foo-invalid').expect(404))
         .then((res) => {
-          expect(res.body).to.eql({ messasge: 'Error retrieving viper rating for nomisId: foo-invalid. The cause was: Invalid body: {"id": "1234", "value": 0.42}' });
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-invalid. The cause was: Invalid body: {"id": "1234", "value": 0.42}',
+          });
         });
     });
 
@@ -299,15 +297,14 @@ describe('/api/viper/:nomisId', () => {
             'Content-Type': 'application/xml',
           },
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-wrong')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql(
-                { messasge: 'Error retrieving viper rating for nomisId: foo-wrong. The cause was: Invalid body: undefined' });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-wrong').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-wrong. The cause was: Invalid body: undefined',
+          });
+        }),
+      );
     });
 
     it('returns a 404 (not found) when no response is receive within timeout limit', function test() {
@@ -325,28 +322,24 @@ describe('/api/viper/:nomisId', () => {
           },
           fixedDelayMilliseconds: 2100,
         },
-      })
-        .then(() =>
-          request(baseUrl).get('/api/viper/foo-timeout')
-            .expect(404)
-            .then((res) => {
-              expect(res.body).to.eql(
-                {
-                  messasge: 'Error retrieving viper rating for nomisId: foo-timeout. The cause was: Not found',
-                });
-            }),
-        );
+      }).then(() =>
+        request(baseUrl).get('/api/viper/foo-timeout').expect(404).then((res) => {
+          expect(res.body).to.eql({
+            messasge:
+              'Error retrieving viper rating for nomisId: foo-timeout. The cause was: Not found',
+          });
+        }),
+      );
     });
   });
 
   it('returns a 404 (not found) for an unknown nomis id', function test() {
     this.timeout(5000);
     const unknownNomisId = uuid().substring(0, 8);
-    return request(baseUrl).get(`/api/viper/${unknownNomisId}`)
-      .expect(404)
-      .then((res) => {
-        expect(res.body).to.eql({ messasge: `Error retrieving viper rating for nomisId: ${unknownNomisId}. The cause was: Not found` });
+    return request(baseUrl).get(`/api/viper/${unknownNomisId}`).expect(404).then((res) => {
+      expect(res.body).to.eql({
+        messasge: `Error retrieving viper rating for nomisId: ${unknownNomisId}. The cause was: Not found`,
       });
+    });
   });
 });
-
