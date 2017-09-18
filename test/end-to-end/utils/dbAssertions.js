e91ce4224cc6832ba3901d7fa701040444d8ae03
Nick Redshaw <nickredshaw@gmail.com>
2017-09-18 15:53:02 +0100

    CSRA-640: Tidy up the database result handling

--- test/end-to-end/utils/dbAssertions.js
+++ test/end-to-end/utils/dbAssertions.js
@@ -1,40 +1,42 @@
 import db from '../../util/db';
 
 export async function checkThatTheOutcomeDataWasWrittenToDatabase({ id, outcome }) {
-  const result = await db.select().table('prisoner_assessments').where('id', Number(id));
+  const dbResult = await db.select()
+    .table('prisoner_assessments')
+    .where('id', Number(id));
+  const result = dbResult[0];
 
-  expect(result[0]).to.not.equal(
+  expect(result).to.not.equal(
     undefined,
     `Did not get a result from database for id: ${id}`,
   );
-  expect(result[0].outcome).to.eql(outcome);
+  expect(result.outcome).to.eql(outcome);
 }
 
 export async function checkThatRiskAssessmentDataWasWrittenToDatabase({ id, riskAssessment }) {
-  const result = await db.select()
+  const dbResult = await db.select()
   .table('prisoner_assessments')
   .where('id', Number(id));
+  const result = dbResult[0];
 
-  expect(result[0]).to.not.equal(
+  expect(result).to.not.equal(
     undefined,
     `Did not get a result from database for id: ${id}`,
   );
-  expect(JSON.parse(result[0].risk_assessment).viperScore).to.eql(riskAssessment.viperScore);
-  expect(JSON.parse(result[0].risk_assessment).outcome).to.eql(riskAssessment.outcome);
-  expect(JSON.parse(result[0].risk_assessment).reasons).to.eql(riskAssessment.reasons);
-  expect(JSON.parse(result[0].risk_assessment).questions).to.eql(riskAssessment.questions);
+  expect(JSON.parse(result.risk_assessment)).to.eql(riskAssessment);
 }
 
 export async function checkThatHealthAssessmentDataWasWrittenToDatabase({ id, healthAssessment }) {
-  const result = await db.select()
+  const dbResult = await db.select()
     .table('prisoner_assessments')
     .where('id', Number(id));
+  const result = dbResult[0];
 
-  expect(result[0]).to.not.equal(
+  expect(result).to.not.equal(
     undefined,
     `Did not get a result from database for id: ${id}`,
   );
-  expect(JSON.parse(result[0].health_assessment)).to.eql(healthAssessment);
+  expect(JSON.parse(result.health_assessment)).to.eql(healthAssessment);
 }
 
 export async function checkThatPrisonerAssessmentDataWasWrittenToDatabase({
@@ -44,19 +46,22 @@ export async function checkThatPrisonerAssessmentDataWasWrittenToDatabase({
   surname,
   dateOfBirth,
 }) {
-  const result = await db.select().table('prisoner_assessments').where('id', Number(id));
+  const dbResult = await db.select()
+    .table('prisoner_assessments')
+    .where('id', Number(id));
+  const result = dbResult[0];
 
-  expect(result[0]).to.not.equal(
+  expect(result).to.not.equal(
     undefined,
     `Did not get a result from database for id: ${id}`,
   );
-  expect(result[0].nomis_id).to.equalIgnoreCase(nomisId);
-  expect(result[0].forename).to.equal(forename);
-  expect(result[0].surname).to.equal(surname);
-  expect(result[0].date_of_birth.toString()).to.contain(dateOfBirth);
-  expect(result[0].questions_hash).to.not.equal(undefined, 'expected a questions_hash');
-  expect(result[0].created_at).to.not.equal(undefined, 'expected a created_at');
-  expect(result[0].updated_at).to.not.equal(undefined, 'expected a created_at');
-  expect(result[0].git_version).to.not.equal(undefined, 'expected a git_version');
-  expect(result[0].git_date).to.not.equal(undefined, 'expected a git_date');
+  expect(result.nomis_id).to.equalIgnoreCase(nomisId);
+  expect(result.forename).to.equal(forename);
+  expect(result.surname).to.equal(surname);
+  expect(result.date_of_birth.toString()).to.contain(dateOfBirth);
+  expect(result.questions_hash).to.not.equal(undefined, 'expected a questions_hash');
+  expect(result.created_at).to.not.equal(undefined, 'expected a created_at');
+  expect(result.updated_at).to.not.equal(undefined, 'expected a created_at');
+  expect(result.git_version).to.not.equal(undefined, 'expected a git_version');
+  expect(result.git_date).to.not.equal(undefined, 'expected a git_date');
 }
