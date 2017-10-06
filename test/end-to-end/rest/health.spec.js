9976c36a2c21e1f752b601c03df3f6cb21bd0faa
Nick Redshaw <nickredshaw@gmail.com>
2017-10-06 14:14:02 +0100

    CSRA-680: Converts the async code to use await and improve checks

--- test/end-to-end/rest/health.spec.js
+++ test/end-to-end/rest/health.spec.js
@@ -2,29 +2,17 @@ import request from 'supertest';
 import { expect } from 'chai';
 
 describe('/health', () => {
-  it('displays the health status of the app', function test(done) {
+  it('displays the health status of the app', async function test() {
     this.timeout(25000);
-    const baseUrl = process.env.APP_BASE_URL;
-    request(baseUrl).get('/health')
-      .expect(200)
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res.body).to.have.property('status')
-          .which.equals('OK');
-        expect(res.body).to.have.property('buildNumber')
-          .which.is.a('string');
-        expect(res.body).to.have.property('gitRef')
-          .which.matches(/^[a-z\d]{40}$/);
-        expect(res.body).to.have.property('gitDate')
-          .which.matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
-        expect(res.body).to.have.property('questionHash')
-          .which.is.an('object');
-        expect(res.body).to.have.property('checks')
-          .which.is.an('object');
-        expect(res.body).to.have.property('checks')
-          .which.is.an('object');
-        expect(res.body).to.have.deep.property('checks.db', 'OK');
-        done();
-      });
+    const result = await request(process.env.APP_BASE_URL).get('/health').expect(200);
+
+    expect(result.body).to.have.property('status').which.equals('OK');
+    expect(result.body).to.have.property('buildNumber').which.is.a('string');
+    expect(result.body).to.have.property('gitRef').which.matches(/^[a-z\d]{40}$/);
+    expect(result.body).to.have.property('gitDate').which.matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
+    expect(result.body).to.have.property('questionHash').which.is.an('object');
+    expect(result.body).to.have.deep.property('checks.db', 'OK');
+    expect(result.body).to.have.deep.property('checks.viperRestService', 'OK');
+    expect(result.body).to.have.deep.property('checks.elite2RestService', 'OK');
   });
 });
