3f1e9bd0cf352389c5df92ec8357b74836907448
Steven Bapaga <sbapaga@gmail.com>
2017-10-16 16:14:20 +0100

    CSRA-747: Ensure that session storage is cleared upon signout

--- client/javascript/main.jsx
+++ client/javascript/main.jsx
@@ -8,3 +8,10 @@ import store from './store';
 import routes from './Router';
 
 render(routes(store), document.getElementById('mountNode'));
+
+// Clear session storage upon sign out
+document.querySelector('[data-element-id="sign-out"]').addEventListener('click', (event) => {
+  event.preventDefault();
+  sessionStorage.clear();
+  window.location = event.target.href;
+});
