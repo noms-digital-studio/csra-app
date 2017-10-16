44a1b30f77ee0f4b4f85f41b407827d726f4b072
Steven Bapaga <sbapaga@gmail.com>
2017-10-16 15:08:04 +0100

    CSRA-688: Add test environmental variables for officer sign in

--- test/end-to-end/browser/tasks/officerSignsIn.task.js
+++ test/end-to-end/browser/tasks/officerSignsIn.task.js
@@ -2,18 +2,30 @@ import LoginPage from '../pages/Login.page';
 import DashboardPage from '../pages/Dashboard.page';
 import { clearPrisonerAssessmentsSync } from '../../utils/dbClear';
 
-function givenThatTheOfficerIsSignedIn({ smokeTest } = {}) {
-  if (!smokeTest) {
+const defaultConfig = {
+  smokeTest: false,
+  userCredentials: {
+    username: process.env.TEST_USER_NAME || 'officer',
+    password: process.env.TEST_USER_PASSWORD || 'password',
+  },
+};
+
+function givenThatTheOfficerIsSignedIn(opts) {
+  const config = { ...defaultConfig, ...opts };
+
+  if (!config.smokeTest) {
     clearPrisonerAssessmentsSync();
   }
 
   expect(LoginPage.mainHeading).to.equal('Sign in');
-  LoginPage.enterUsername('officer');
-  LoginPage.enterUserPassword('password');
+
+  LoginPage.enterUsername(config.userCredentials.username);
+  LoginPage.enterUserPassword(config.userCredentials.password);
   LoginPage.clickContinue();
+
   expect(DashboardPage.headerUsername).to.equal('John Smith');
 
-  if (!smokeTest) {
+  if (!config.smokeTest) {
     expect(DashboardPage.mainHeading).to.equal('There is no one to assess.');
   }
 }
