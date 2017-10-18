391db7054a0c2d507a5e7cbc0eec80c6fa92754f
Steven Bapaga <sbapaga@gmail.com>
2017-10-18 15:19:35 +0100

    CSRA-695: Add backup loggin strategy in case AppInsights blows up

--- server/services/event-logger.js
+++ server/services/event-logger.js
@@ -1,11 +1,22 @@
-const appInsights = require('../../utils/applicationinsights');
+let trackEvent;
 
-const trackEvent = (name, properties) => {
-  appInsights.client.trackEvent({
-    name,
-    properties,
-  });
-};
+// App insights blows up in a non server environment
+try {
+  // eslint-disable-next-line
+  const appInsights = require('../../utils/applicationinsights');
+  trackEvent = (name, properties = {}) => {
+    appInsights.client.trackEvent({
+      name,
+      properties,
+    });
+  };
+} catch (exception) {
+  // eslint-disable-next-line
+  const { logger } = require('./logger');
+  trackEvent = (name, properties = {}) => {
+    logger.info(`Event ${name},  ${JSON.stringify(properties)}`);
+  };
+}
 
 
 module.exports = trackEvent;
