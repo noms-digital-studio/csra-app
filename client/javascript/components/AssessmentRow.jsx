f80603d2eecacf6fbc86f136b26f44ab3088c53e
Steven Bapaga <sbapaga@gmail.com>
2017-10-26 17:12:14 +0100

    CSRA-756: Disallow risk assessment sigular view when both assessments are complete

--- client/javascript/components/AssessmentRow.jsx
+++ client/javascript/components/AssessmentRow.jsx
@@ -1,82 +1,93 @@
 import React, { PropTypes } from 'react';
+import not from 'ramda/src/not';
+import classnames from 'classnames';
 
 import { capitalize, extractDateFromUTCString } from '../utils';
 
-const AssessmentRow = props => (
-  <tr
-    data-element-id={`profile-row-${props.nomisId}`}
-    key={props.id}
-    data-assessment-id={props.id}
-    data-assessments-complete={props.healthAssessmentCompleted && props.riskAssessmentCompleted}
-  >
-    <td>
-      <span className="c-profile-holder" />
-    </td>
-    <td>
-      {props.forename} {props.surname}
-    </td>
-    <td>{props.nomisId}</td>
-    <td>{extractDateFromUTCString(props.dateOfBirth)}</td>
-    <td data-risk-assessment-complete={props.riskAssessmentCompleted}>
-      {props.riskAssessmentCompleted ? (
-        <button
-          onClick={() => props.onRiskAssessmentSelect(props)}
-          type="button"
-          data-element-id={`completed-csra-link-${props.nomisId}`}
-          className="link u-link">Complete</button>
-        ) : (
+const AssessmentRow = (props) => {
+  const bothAssessmentsComplete = props.healthAssessmentCompleted && props.riskAssessmentCompleted;
+  const completedLinkCSS = classnames({
+    'link': not(bothAssessmentsComplete),
+    'u-link': not(bothAssessmentsComplete),
+    'u-text': bothAssessmentsComplete,
+  });
+
+  return (
+    <tr
+      data-element-id={`profile-row-${props.nomisId}`}
+      key={props.id}
+      data-assessment-id={props.id}
+      data-assessments-complete={bothAssessmentsComplete}
+    >
+      <td>
+        <span className="c-profile-holder" />
+      </td>
+      <td>
+        {props.forename} {props.surname}
+      </td>
+      <td>{props.nomisId}</td>
+      <td>{extractDateFromUTCString(props.dateOfBirth)}</td>
+      <td data-risk-assessment-complete={props.riskAssessmentCompleted}>
+        {props.riskAssessmentCompleted ? (
           <button
+            onClick={() => not(bothAssessmentsComplete) && props.onRiskAssessmentSelect(props)}
             type="button"
-            onClick={() => props.onOffenderSelect(props)}
-            className="link u-link"
-            data-element-id={`start-csra-link-${props.nomisId}`}
-          >
-            Start
-          </button>
-        )}
-    </td>
-    <td data-health-assessment-complete={props.healthAssessmentCompleted}>
-      {props.healthAssessmentCompleted ? (
-        <span>Complete</span>
-        ) : (
-          <button
-            type="button"
-            onClick={() =>
-              props.onOffenderHealthcareSelect({
-                ...props,
-                isAwaitingSubmission: props.isAwaitingSubmission(props.id),
-              })}
-            className="link u-link"
-            data-element-id={`start-healthcare-link-${props.nomisId}`}
-          >
-            Start
-          </button>
-        )}
-    </td>
-    <td className="u-text-align-center">
-      {props.outcome ? (
-        <span>{capitalize(props.outcome)}</span>
-        ) : (
-          <span className="c-status-indicator" />
-        )}
-    </td>
-    <td data-element-id="view-outcome" className="u-text-align-center">
-      {props.outcome ? (
-        <span>
-          <button
-            className="link u-link"
-            onClick={() => props.onViewOutcomeClick(props)}
-            data-element-id={`view-outcome-link-${props.nomisId}`}
-          >
-              View
+            data-element-id={`completed-csra-link-${props.nomisId}`}
+            className={completedLinkCSS}>Complete</button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => props.onOffenderSelect(props)}
+              className="link u-link"
+              data-element-id={`start-csra-link-${props.nomisId}`}
+            >
+              Start
             </button>
-        </span>
-        ) : (
-          <span />
-        )}
-    </td>
-  </tr>
+          )}
+      </td>
+      <td data-health-assessment-complete={props.healthAssessmentCompleted}>
+        {props.healthAssessmentCompleted ? (
+          <span>Complete</span>
+          ) : (
+            <button
+              type="button"
+              onClick={() =>
+                props.onOffenderHealthcareSelect({
+                  ...props,
+                  isAwaitingSubmission: props.isAwaitingSubmission(props.id),
+                })}
+              className="link u-link"
+              data-element-id={`start-healthcare-link-${props.nomisId}`}
+            >
+              Start
+            </button>
+          )}
+      </td>
+      <td className="u-text-align-center">
+        {props.outcome ? (
+          <span>{capitalize(props.outcome)}</span>
+          ) : (
+            <span className="c-status-indicator" />
+          )}
+      </td>
+      <td data-element-id="view-outcome" className="u-text-align-center">
+        {props.outcome ? (
+          <span>
+            <button
+              className="link u-link"
+              onClick={() => props.onViewOutcomeClick(props)}
+              data-element-id={`view-outcome-link-${props.nomisId}`}
+            >
+                View
+              </button>
+          </span>
+          ) : (
+            <span />
+          )}
+      </td>
+    </tr>
   );
+};
 
 AssessmentRow.propTypes = {
   id: PropTypes.number,
