a8d9e3ce825f3d86853def1444f256d25138f7dc
Steven Bapaga <sbapaga@gmail.com>
2017-12-20 17:08:44 +0000

    Update rest test

--- test/end-to-end/rest/signIn.spec.js
+++ test/end-to-end/rest/signIn.spec.js
@@ -9,9 +9,15 @@ describe('/signin', () => {
     const result = await agent.post('/signin').send('username=officer&password=password').expect(302);
     expect(result.headers.location).to.eql('/');
     expect(result.headers['set-cookie'][0]).to.include('express:sess=');
-    const userDetails = { forename: 'John', surname: 'Smith', username: 'officer', email: 'officer@hmpps.org', authorities: [{ authority: 'ROLE_666' }] };
-    const userDetailsString = JSON.stringify({ passport: { user: userDetails } });
-    expect(parsePassportSessionCookie(result)).to.equal(userDetailsString);
+    const passportSessionCookie = JSON.parse(parsePassportSessionCookie(result));
+    const user = passportSessionCookie.passport.user;
+
+    expect(user.forename).to.equal('John');
+    expect(user.surname).to.equal('Smith');
+    expect(user.username).to.equal('officer');
+    expect(user.email).to.equal('officer@hmpps.org');
+    expect(user.authorities).to.eql([{ authority: 'ROLE_666' }]);
+    expect(user.eliteAuthorisationToken).to.include('Bearer');
   });
 
   it('returns unauthorised when user creds are wrong', async () => {
